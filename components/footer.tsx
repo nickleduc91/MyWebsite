eaf188ac264799cd0b3cc184fe5ca8160667f8ab
nickleduc91 <122701320+nickleduc91@users.noreply.github.com>
2023-02-20 17:58:25 -0500

    fix

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="p-4 bg-black shadow md:flex md:items-center md:justify-between md:p-6 ">
       <div className="flex flex-wrap items-center mt-3 text-md text-gray-400 sm:mt-0">
-        Made by <span className="text-indigo-600">Nicholas Leduc</span>
+        Made by <span className="text-indigo-600"> Nicholas Leduc</span>
       </div>
     </footer>
   )
